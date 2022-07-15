c3afe54731853bc04f152ca98e60bac5d6add70c
Cathy Dao <cathydao95@gmail.com>
2022-07-14 21:58:16 -0700

    map through each letter and determine classname

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,18 +1,31 @@
-function Letter({ individualLetter }) {
-  console.log(individualLetter);
+function Letter({ individualLetter, checkedAnswer }) {
+  console.log(individualLetter, checkedAnswer);
 
-  let color;
-
-  //   if letter is in correct position, color is green, if letter is valid, color yelow
-  if (individualLetter.validPosition) {
-    color = "green letter";
-  } else if (individualLetter.validLetter) {
-    color = "yellow letter";
-  } else {
-    color = "letter";
-  }
   //   display individual letter
-  return <div className={color}>{individualLetter.letter.toUpperCase()}</div>;
+  return (
+    <div className={checkedAnswer || "letter"}>
+      {individualLetter.toUpperCase()}
+    </div>
+  );
 }
 
 export default Letter;
+
+// function Letter({ individualLetter, checkedAnswer }) {
+//   console.log(individualLetter, checkedAnswer);
+
+//   let color;
+
+//   //   if letter is in correct position, color is green, if letter is valid, color yelow
+//   if (individualLetter.validPosition) {
+//     color = "green letter";
+//   } else if (individualLetter.validLetter) {
+//     color = "yellow letter";
+//   } else {
+//     color = "grey letter";
+//   }
+//   //   display individual letter
+//   return <div className={color}>{individualLetter.letter.toUpperCase()}</div>;
+// }
+
+// export default Letter;
