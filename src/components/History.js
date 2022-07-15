0f5494466a837b119b86e82ff6f6e68f1a218492
Cathy Dao <cathydao95@gmail.com>
2022-07-14 21:58:01 -0700

    check for duplicates

--- src/components/History.js
+++ src/components/History.js
@@ -1,30 +1,71 @@
 import Letter from "./Letter";
 function History({ submittedWord, word }) {
+  // word of the day split into an array of letters
   const answer = word.split("");
   console.log("answer", answer);
+  const checkedAnswer = answer.map((item) => "");
 
-  const guessedWord = [];
+  // gussed word split into an array of letters
+  const guessedWord = submittedWord.split("");
 
-  // turn guessed word into an array of objects with letter, validLetter, and validPosition
-  submittedWord.split("").forEach((letter, i) => {
-    guessedWord.push({
-      letter: letter,
-      // returns true, if letter is valid
-      validLetter: answer.includes(letter),
-      // returns true if letter is in correct position
-      validPosition: answer[i] === submittedWord[i],
-    });
-  });
-  console.log(guessedWord);
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === guessedWord[i]) {
+      if (!checkedAnswer[i]) {
+        checkedAnswer[i] = "green letter";
+      }
+    } else if (guessedWord.includes(answer[i])) {
+      const targetIndex = guessedWord.indexOf(answer[i]);
+      if (!checkedAnswer[targetIndex]) {
+        checkedAnswer[targetIndex] = "yellow letter";
+      }
+    }
+  }
+
+  console.log(checkedAnswer);
 
   return (
     // map through each word and display individual letter
     <div className="word">
       {guessedWord.map((letter, i) => (
-        <Letter key={i} individualLetter={letter} />
+        <Letter
+          key={i}
+          individualLetter={letter}
+          checkedAnswer={checkedAnswer[i]}
+        />
       ))}
     </div>
   );
+
+  // const answer = word.split("");
+  // console.log("answer", answer);
+  // const checkedAnswer = answer.map((item) => false);
+
+  // const guessedWord = [];
+
+  // // turn guessed word into an array of objects with letter, validLetter, and validPosition
+  // submittedWord.split("").forEach((letter, i) => {
+  //   guessedWord.push({
+  //     letter: letter,
+  //     // returns true, if letter is valid
+  //     validLetter: answer.includes(letter),
+  //     // returns true if letter is in correct position
+  //     validPosition: answer[i] === submittedWord[i],
+  //   });
+  // });
+  // console.log(guessedWord);
+
+  // return (
+  //   // map through each word and display individual letter
+  //   <div className="word">
+  //     {guessedWord.map((letter, i) => (
+  //       <Letter
+  //         key={i}
+  //         individualLetter={letter}
+  //         checkedAnswer={checkedAnswer}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 
 export default History;
