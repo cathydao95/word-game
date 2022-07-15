c2bbe0761e28fa400a4779062600da31e49bc198
Cathy Dao <cathydao95@gmail.com>
2022-07-14 22:10:54 -0700

    update remaining equation

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,10 +1,10 @@
 function GameInfo({ remaining, word, match }) {
   let message;
   if (match) {
-    message = `You correctly guessed the word in  ${remaining} tries`;
+    message = `You correctly guessed the word in  ${6 - remaining} tries`;
     // if not matched have guesses remaining
   } else if (remaining > 0 && !match) {
-    message = `You have ${remaining} guesses remaning`;
+    message = `You have ${remaining} guesses remaining`;
     // if no more guesses remaining, display word
   } else {
     message = `The word was ${word}`;
