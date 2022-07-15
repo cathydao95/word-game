0a4cc0dd96aebc30b25da5c83047725285291a97
Cathy Dao <cathydao95@gmail.com>
2022-07-14 19:23:02 -0700

    add message conditions to display

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -2,8 +2,10 @@ function GameInfo({ remaining, word, match }) {
   let message;
   if (match) {
     message = `You correctly guessed the word in  ${remaining} tries`;
+    // if not matched have guesses remaining
   } else if (remaining > 0 && !match) {
     message = `You have ${remaining} guesses remaning`;
+    // if no more guesses remaining, display word
   } else {
     message = `The word was ${word}`;
   }
